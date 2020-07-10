bd2242f2d079c5df11ba3ef61c5d549fcdd31131
parasmani300 <parasmani300@gmail.com>
2020-07-10 15:33:52 +0530

    Vession 1.0

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+console.log("Working on fetching javasript Yola!!!!!!!")
+
+
+
+const form = document.querySelector('form')
+const search = document.querySelector('input')
+
+const movieSearch = form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const movie = search.value
+    url = "http://localhost:3000/movie?title=" + movie
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            movie_detail = data.body
+            if (movie_detail.Response === "False") {
+                document.getElementById("movie_details").textContent = "Search not found"
+            } else {
+                console.log(movie_detail)
+                document.getElementById("movie_details").textContent = JSON.stringify(movie_detail)
+            }
+
+
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const request = require('request')
+const express = require('express')
+const hbs = require('hbs')
+const { static } = require('express')
+
+const app = express()
+
+const publicDirectory = path.join(__dirname, '../public')
+const ViewDirectory = path.join(__dirname, '../templates/views')
+const partialDirectory = path.join(__dirname, "../templates/partials")
+
+app.set('view engine', 'hbs')
+app.set('views', ViewDirectory)
+hbs.registerPartials(partialDirectory)
+
+
+
+app.use(express.static(publicDirectory))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        'title': 'SOme Title'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/movie', (req, res) => {
+    if (!req.query.title) {
+        return res.send({
+            'error': 'Provide with correct title of the movie to continue'
+        })
+    }
+    else {
+        url = "http://www.omdbapi.com/?t=" + req.query.title + "&apikey=c6e76828"
+        request({ url: url, json: true }, (error, { body }) => {
+            if (error) {
+                return res.send({ error })
+            }
+            return res.send({ body })
+        })
+    }
+
+})
+
+// This is always supposed to come last
+app.get("*", (req, res) => {
+    res.render('404', {
+        'title': 'Some error 404',
+        'errorMsg': 'OO some error occured'
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Server up and running on port 3000")
+})
